0ac8c9fb89563f9dbcb08fcdf6c1f05411f08f9f
Ahmed_https <73763162+Ahmed07-prog@users.noreply.github.com>
2022-08-30 17:53:12 +0100

    Update User.svelte

--- src/pages/User.svelte
+++ src/pages/User.svelte
@@ -47,58 +47,58 @@
     height: 20px;
     margin-bottom: 70px;
     }
-    .card {
-    margin-right: 50px;
-background-color: #8e8e8e;
-width: 300px;
-height: 500px;
-display: flex;
-align-items: center;
-border-radius: 30px;
-overflow: hidden;
-box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
+.card {
+	margin-right: 50px;
+	background-color: #8e8e8e;
+	width: 300px;
+	height: 500px;
+	display: flex;
+	align-items: center;
+	border-radius: 30px;
+	overflow: hidden;
+	box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
 }
 .avatar {
-background-color: #88c0d0;
-position: absolute;
-color: white;
-width: 150px;
-height: 150px;
-text-align: center;
-border-radius: 50%;
-margin-top: 70px;
-display: flex;
-justify-content: center;
-align-items: center;
-border: 3px white solid;
-font-size: 40px;
+	background-color: #88c0d0;
+	position: absolute;
+	color: white;
+	width: 150px;
+	height: 150px;
+	text-align: center;
+	border-radius: 50%;
+	margin-top: 70px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 3px white solid;
+	font-size: 40px;
 }
 .card .info {
     margin-top: 150px;
-background-color: #f4f4f6;
-width: 101%;
-text-align: center;
-border-top-left-radius: 45%;
-border-top-right-radius: 45%;
-height: 65%;
+	background-color: #f4f4f6;
+	width: 101%;
+	text-align: center;
+	border-top-left-radius: 45%;
+	border-top-right-radius: 45%;
+	height: 65%;
 }
 .parent {
-margin-top: 50px;
-margin-bottom: 1rem;
-font-size: 30px;
-text-transform: uppercase;
-color: #4c566a;
+	margin-top: 50px;
+	margin-bottom: 1rem;
+	font-size: 30px;
+	text-transform: uppercase;
+	color: #4c566a;
 }
 .btn {
-		background-color: transparent;
-		color:#132a6a;
-		border-color: #ef5f55;
+	background-color: transparent;
+	color:#132a6a;
+	border-color: #ef5f55;
 	}
 
 .btn:hover{
     background-color: #ef5f55;
-		color:white;
-		border-color: #132a6a;
+	color:white;
+	border-color: #132a6a;
 }
 
 
