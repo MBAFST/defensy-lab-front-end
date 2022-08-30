a854aaa55b698bfdfd2b3f415ae9bf011a4ab60c
Ahmed07-prog <ahmed.rafiko949@gmail.com>
2022-08-30 17:47:35 +0100

    User Interface (UI v1.0)

--- src/pages/User.svelte
+++ src/pages/User.svelte
@@ -3,17 +3,103 @@
     import axios from "axios";
     import { authenticated } from '../store/auth';
 
-    let message: String;
+    let id: String;
+    let name:String;
+    let email:String;
+    let username:String;
 
     onMount(async () => {
         const { data } = await axios.get("user");
-
-        message = `<h3>Hi ${data["first-name"]} ${data["last-name"]}</h3>`;
+        id = ` <p>#00${data ["id"]} </p>`;
+        email = `${data ["email"]} `;
+        name = `<p>${data["first-name"]} ${data["last-name"]}  </p>`;
+        username = ` ${data["username"]}`
         authenticated.set(true);
     });
 </script>
 
-<div class="container mt-5 text-center">
-    {@html message}
-</div>
-<style></style>
+<main>
+    <div class="card"> 
+        <span></span>
+        <div class="avatar">
+            {@html id}
+        </div>
+        <div class="info">
+            <p class="parent">{@html name}</p>
+            <p>Username : {@html username } </p>
+            <p>E-mail : {@html email}</p>
+            <button class="btn">View Rapport</button>
+        </div>
+    </div>
+</main>
+    
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+    }
+    span {
+    margin-top: 15px;
+    border-radius: 8px;
+    width: 100px;
+    background-color: white;
+    height: 20px;
+    margin-bottom: 70px;
+    }
+    .card {
+    margin-right: 50px;
+background-color: #8e8e8e;
+width: 300px;
+height: 500px;
+display: flex;
+align-items: center;
+border-radius: 30px;
+overflow: hidden;
+box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
+}
+.avatar {
+background-color: #88c0d0;
+position: absolute;
+color: white;
+width: 150px;
+height: 150px;
+text-align: center;
+border-radius: 50%;
+margin-top: 70px;
+display: flex;
+justify-content: center;
+align-items: center;
+border: 3px white solid;
+font-size: 40px;
+}
+.card .info {
+    margin-top: 150px;
+background-color: #f4f4f6;
+width: 101%;
+text-align: center;
+border-top-left-radius: 45%;
+border-top-right-radius: 45%;
+height: 65%;
+}
+.parent {
+margin-top: 50px;
+margin-bottom: 1rem;
+font-size: 30px;
+text-transform: uppercase;
+color: #4c566a;
+}
+.btn {
+		background-color: transparent;
+		color:#132a6a;
+		border-color: #ef5f55;
+	}
+
+.btn:hover{
+    background-color: #ef5f55;
+		color:white;
+		border-color: #132a6a;
+}
+
+
+</style>
